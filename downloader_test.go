ff0c0a352b50ef27bf5402f6e87d7c35ca31241a
zhangcan <zhangcan@xuaoshouyi.com>
2019-05-15 07:50:35 +0800

    downlerUT92%

--- downloader_test.go
+++ downloader_test.go
@@ -3,19 +3,20 @@ package webcrawler
 import (
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
 var (
 	testDoc = `<html>
-	<body>
-	
-	<h1>My First Heading</h1>
-	
-	<p>My first paragraph.</p>
-	
-	</body>
-	</html>`
+		<body>
+
+		<h1>My First Heading</h1>
+
+		<p>My first paragraph.</p>
+
+		</body>
+		</html>`
 )
 
 func Test_randomUserAgent(t *testing.T) {
@@ -28,7 +29,7 @@ func Test_randomUserAgent(t *testing.T) {
 	expect(t, ((ua1 == ua2) && (ua2 == ua3) && (ua3 == ua4)), false)
 }
 
-func Test_Downloader_get(t *testing.T) {
+func Test_Downloader_get_Success(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusOK)
 		rw.Write([]byte(testDoc))
@@ -36,6 +37,7 @@ func Test_Downloader_get(t *testing.T) {
 	}))
 	defer ts.Close()
 
+	// 正确请求的情况
 	var api = ts.URL
 	res, err := get(api)
 
@@ -44,15 +46,28 @@ func Test_Downloader_get(t *testing.T) {
 
 	defer res.Body.Close()
 
-	buf := make([]byte, len(testDoc))
-	count, err := res.Body.Read(buf)
+	buf := make([]byte, len([]byte(testDoc)))
+	_, err = res.Body.Read(buf)
 
-	expect(t, err, nil)
-	expect(t, count, len(testDoc))
-
+	expect(t, err.Error(), "EOF")
+	expect(t, string(buf), testDoc)
 }
 
-func Test_DownloadSuccess(t *testing.T) {
+func Test_Downloader_get_Failed(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte(testDoc))
+		expect(t, r.Method, "GET")
+	}))
+	defer ts.Close()
+
+	res, err := get("aasdadasdasdass")
+
+	refute(t, reflect.ValueOf(err).IsNil(), true)
+	expect(t, reflect.ValueOf(res).IsNil(), true)
+}
+
+func Test_Downloader_DownloadSuccess(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusOK)
 		rw.Write([]byte(testDoc))
@@ -72,4 +87,25 @@ func Test_DownloadSuccess(t *testing.T) {
 	}
 	refute(t, doc, nil)
 	expect(t, url, api)
+	h1Text := doc.Find("h1").Text()
+	expect(t, h1Text, "My First Heading")
+}
+
+func Test_Downloader_DownloadFailed(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte(testDoc))
+		expect(t, r.Method, "GET")
+	}))
+	defer ts.Close()
+
+	docReceiver := RunDownloader()
+	var api = ts.URL
+	Download(api + "failed")
+
+	resultFunc := <-docReceiver
+	doc, _, err := resultFunc()
+
+	expect(t, reflect.ValueOf(err).IsNil(), false)
+	expect(t, reflect.ValueOf(doc).IsNil(), true)
 }
